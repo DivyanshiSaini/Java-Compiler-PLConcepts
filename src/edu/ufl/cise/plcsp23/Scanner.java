0379a4eaa2b744d752124f328b75a34e32f4f4a0
DivyanshiSaini <sainid@ufl.edu>
2023-02-07 12:36:01 -0500

    WhiteSpace works

--- src/edu/ufl/cise/plcsp23/Scanner.java
+++ src/edu/ufl/cise/plcsp23/Scanner.java
@@ -2,10 +2,7 @@ package edu.ufl.cise.plcsp23;
 
 import java.util.Arrays;
 
-import static edu.ufl.cise.plcsp23.IToken.Kind.EOF;
-import static edu.ufl.cise.plcsp23.IToken.Kind.NUM_LIT;
-import static edu.ufl.cise.plcsp23.IToken.Kind.TIMES;
-import static edu.ufl.cise.plcsp23.IToken.Kind.IDENT;
+
 import static edu.ufl.cise.plcsp23.IToken.Kind;
 import static java.util.Arrays.copyOf;
 
@@ -26,6 +23,8 @@ public class Scanner implements IScanner {
         inputChars = Arrays.copyOf(input.toCharArray(), input.length() + 1);
         pos = 0;
         ch = inputChars[pos];
+
+
     }
 
     void nextChar() {
@@ -35,16 +34,10 @@ public class Scanner implements IScanner {
         ch = inputChars[pos];
     }
 
-    boolean isZero(char c) {
-        if (c == '0'){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
     boolean isDigit(char c){
-        if(c == '1'|| c=='2' || c=='3' || c=='4'|| c=='5' || c=='6'|| c=='7'|| c=='8' || c=='9'){
+        if(c == '1'|| c=='2' || c=='3' || c=='4'|| c=='5'){
+        return true;
+        } else if(c=='6'|| c=='7'|| c=='8' || c=='9'){
             return true;
         }
         else {
@@ -62,11 +55,14 @@ public class Scanner implements IScanner {
         System.out.println(s);
     };
 
+
     private enum State {
         START,
         HAVE_EQ,
         IN_IDENT,
         IN_NUM_LIT
+
+
     }
 
     @Override
@@ -80,33 +76,45 @@ public class Scanner implements IScanner {
                     tokenStart = pos;
                     switch (ch) {
                         case 0 -> { //end of input
-                            return new Token(EOF, tokenStart, 0, inputChars);
+                            return new Token(Kind.EOF, tokenStart, 0, inputChars);
                         }
-                        case ' ', '\n', '\r', '\t','f' -> nextChar();
-                        case '*' -> {
-                            nextChar();
-                            return new Token(TIMES, tokenStart, 1, inputChars);
-                        }
-                        case '0' -> {
-                            nextChar();
-                            return new Token(NUM_LIT, tokenStart, 1, inputChars);
-                        }
-                        case '=' -> {
-                            state = State.HAVE_EQ;
-                            nextChar();
-                        }
-                        case '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {//char is nonzero digit
-                            state = State.IN_NUM_LIT;
-                            nextChar();
-                        }
-                        default -> {
-                            if (isIdentStart(ch)) {
-                                state = State.IN_IDENT;
+                        case ' ', '\n', '\r', '\t','\f' -> nextChar();
+
+                            case '*' -> {
                                 nextChar();
-                            } else error("illegal char with ascii value: " + (int) ch);
-                        }
+                                return new Token(Kind.TIMES, tokenStart, 1, inputChars);
+                            }
+                            case '0' -> {
+                                nextChar();
+                                return new Token(Kind.NUM_LIT, tokenStart, 1, inputChars);
+                            }
+                            case '=' -> {
+                                state = State.HAVE_EQ;
+                                nextChar();
+                            }
+                            /*case '==' -> {
+                                state = State.;
+                                nextChar();
+                            }*/
+
+
+
+
+
+                            case '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {//char is nonzero digit
+                                state = State.IN_NUM_LIT;
+                                nextChar();
+                            }
+                            default -> {
+                                if (isIdentStart(ch)) {
+                                    state = State.IN_IDENT;
+                                    nextChar();
+                                } else error("illegal char with ascii value: " + (int) ch);
+                            }
+
                     }
                 }
+
                 case HAVE_EQ -> {
                     if (ch == '=') {
                         state = state.START;
